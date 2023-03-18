024360c2cb366cee40ad13af013153d729344533
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2023-03-18 01:26:19 +0000

    Updated PDF

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '',
+  site:
+    // Use `astro preview` locally to build and serve like on prod, and generate PDF
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://alexis-lucas.vercel.app/',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
